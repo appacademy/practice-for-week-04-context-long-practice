bc3e068eefe76487048b3f48fbd3f8f937e27d85
Randy Goldsmith <randyg@Randys-MacBook-Pro.local>
2023-06-10 02:40:06 -0500

    finished excercise

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,5 +1,22 @@
 // Your code here
+class PartyPlanner {
+	constructor(guestList = [])  {
+		this.guestList = guestList;
+	}
 
+	addToGuestList(name) {
+		this.guestList.push(name);
+	}
+
+	throwParty() {
+		if(this.guestList.length === 0) {
+			return 'Gotta add people to the guest list';
+		} else {
+			let guests = this.guestList.join(' and ');
+			return `Welcome to the party ${guests}`;
+		}
+	}
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
